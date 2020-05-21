f25538c096451c24eae9e6af4106b64741906ce8
wyljpn <37242439+wyljpn@users.noreply.github.com>
2020-05-21 23:32:49 +0900

    2020.5.21 每日一题

---
+++ Dynamic_Programming/5. 最长回文子串.py
@@ -0,0 +1,59 @@
+# 5. 最长回文子串
+#
+# 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+#
+# 示例 1：
+#
+# 输入: "babad"
+# 输出: "bab"
+# 注意: "aba" 也是一个有效答案。
+
+
+class Solution(object):
+    # 方法一：动态规划
+    def longestPalindrome(self, s):
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = ""
+        # 枚举子串的长度 l+1
+        for l in range(n):
+            # 枚举子串的起始位置 i，这样可以通过 j=i+l 得到子串的结束位置
+            # 是竖着更新dp矩阵的.因为要查看dp[i+1][j-]是否为回文
+            for i in range(n):
+                j = i + l
+                if j >= len(s):
+                    break
+                # 每个子串开头第一个位置都是回文
+                if l == 0:
+                    dp[i][j] = True
+                elif l == 1:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
+                if dp[i][j] and l + 1 > len(ans):
+                    ans = s[i:j + 1]
+        return ans
+
+    # 方法二：中心扩展算法
+    # 我们枚举所有的「回文中心」并尝试「扩展」，直到无法扩展为止，此时的回文串长度即为此「回文中心」下的最长回文串长度。我们对所有的长度求出最大值，即可得到最终的答案。
+    def expandAroundCenter(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return left + 1, right - 1
+
+    def longestPalindrome_2(self, s: str) -> str:
+        start, end = 0, 0
+        for i in range(len(s)):
+            # 以s[i]为中心
+            left1, right1 = self.expandAroundCenter(s, i, i)
+            # 以s[i:i+2]为中心
+            left2, right2 = self.expandAroundCenter(s, i, i + 1)
+            if right1 - left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end - start:
+                start, end = left2, right2
+        return s[start: end + 1]
+
+    
+    
