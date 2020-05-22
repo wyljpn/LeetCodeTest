dcd01c231bc5fbe09fa83f04d639625e5e1e85dd
wyljpn <37242439+wyljpn@users.noreply.github.com>
2020-05-22 16:07:11 +0900

    2020.5.22 每日一题

---
+++ Tree/105. 从前序与中序遍历序列构造二叉树.py
@@ -0,0 +1,90 @@
+# 105. 从前序与中序遍历序列构造二叉树
+#
+# 根据一棵树的前序遍历与中序遍历构造二叉树。
+#
+# 注意:
+# 你可以假设树中没有重复的元素。
+#
+# 例如，给出
+#
+# 前序遍历 preorder = [3,9,20,15,7]
+# 中序遍历 inorder = [9,3,15,20,7]
+# 返回如下的二叉树：
+#
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+
+    # 方法二：迭代
+    # 我们用一个栈和一个指针辅助进行二叉树的构造。初始时栈中存放了根节点（前序遍历的第一个节点），指针指向中序遍历的第一个节点；
+    #
+    # 我们依次枚举前序遍历中除了第一个节点以外的每个节点。
+    # 如果 index 恰好指向栈顶节点，那么我们不断地弹出栈顶节点并向右移动 index，并将当前节点作为最后一个弹出的节点的右儿子；
+    # 如果 index 和栈顶节点不同，我们将当前节点作为栈顶节点的左儿子；
+    #
+    # 无论是哪一种情况，我们最后都将当前的节点入栈。
+    def buildTree(self, preorder, inorder):
+        if not preorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        stack = [root]
+        inorderIndex = 0
+        for i in range(1, len(preorder)):
+            preorderVal = preorder[i]
+            node = stack[-1]
+            # 如果 index 和栈顶节点不同，我们将当前节点作为栈顶节点的左儿子；
+            if node.val != inorder[inorderIndex]:
+                node.left = TreeNode(preorderVal)
+                stack.append(node.left)
+            else:
+                # 如果 index 恰好指向栈顶节点，那么我们不断地弹出栈顶节点并向右移动 index，
+                while stack and stack[-1].val == inorder[inorderIndex]:
+                    node = stack.pop()
+                    inorderIndex += 1
+                # 并将当前节点作为最后一个弹出的节点的右儿子
+                node.right = TreeNode(preorderVal)
+                stack.append(node.right)
+
+        return root
+
+    # 方法一：递归
+    def buildTree_2(self, preorder, inorder):
+        def myBuildTree(preorder_left: int, preorder_right: int, inorder_left: int, inorder_right: int):
+            if preorder_left > preorder_right:
+                return None
+
+            # 前序遍历中的第一个节点就是根节点
+            preorder_root = preorder_left
+            # 在中序遍历中定位根节点
+            inorder_root = index[preorder[preorder_root]]
+
+            # 先把根节点建立出来
+            root = TreeNode(preorder[preorder_root])
+            # 得到左子树中的节点数目
+            size_left_subtree = inorder_root - inorder_left
+            # 递归地构造左子树，并连接到根节点
+            # 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+            root.left = myBuildTree(preorder_left + 1, preorder_left + size_left_subtree, inorder_left,
+                                    inorder_root - 1)
+            # 递归地构造右子树，并连接到根节点
+            # 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+            root.right = myBuildTree(preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1,
+                                     inorder_right)
+            return root
+
+        n = len(preorder)
+        # 构造哈希映射，帮助我们快速定位根节点
+        index = {element: i for i, element in enumerate(inorder)}
+        return myBuildTree(0, n - 1, 0, n - 1)
