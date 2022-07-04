8aabfe693c398d2ed77c89c08568baaff88947d5
wyljpn <wyljpn@gmail.com>
2022-07-04 23:30:02 +0900

    New solution for 904

--- Array/904. Fruit Into Baskets.py
+++ Array/904. Fruit Into Baskets.py
@@ -24,10 +24,62 @@ class Solution(object):
         :type fruits: List[int]
         :rtype: int
         """
+        import collections
 
-        slow, fast = 0, 0
-        res = float("inf")
-        backet = []
+        slow = 0
+        res = float("-inf")
+        baskets = collections.Counter()
 
-        for fruit in fruits:
-            
+        for fast in range(len(fruits)):
+            baskets[fruits[fast]] += 1
+
+            # print("baskets: ", baskets)
+
+            while len(baskets) > 2:
+                # print("slow: ", slow)
+                # print("baskets: ", baskets)
+                baskets[fruits[slow]] -= 1
+                # print("baskets after pop: ", baskets)
+                if not baskets[fruits[slow]]:
+                    baskets.pop(fruits[slow])
+                slow += 1
+
+            # print("baskets outside: ", baskets)
+            res = max(res, fast - slow + 1)
+            # print("res: ", res)
+
+        return 0 if res == float("-inf") else res
+
+
+    def totalFruit_2(self, fruits):
+
+        slow = 0
+        dic = {}
+        res = float("-inf")
+
+        for fast in range(len(fruits)):
+            if fruits[fast] in dic:
+                dic[fruits[fast]] += 1
+            else:
+                dic[fruits[fast]] = 1
+
+            while len(dic) > 2:
+                dic[fruits[slow]] -= 1
+                if dic[fruits[slow]] == 0:
+                    del dic[fruits[slow]]
+                slow += 1
+
+            res = max(res, fast - slow + 1)
+
+        return res if res != float("-inf") else 0
+
+
+
+if __name__ =="__main__":
+    so = Solution()
+    print(so.totalFruit([1, 2, 1]))
+    print(so.totalFruit_2([1, 2, 1]))
+    print(so.totalFruit([0, 1, 2, 2]))
+    print(so.totalFruit_2([0, 1, 2, 2]))
+    print(so.totalFruit([1, 2, 3, 2, 2]))
+    print(so.totalFruit_2([1, 2, 3, 2, 2]))
