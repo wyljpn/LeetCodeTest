27305e4d37e3c3ddde3187feb26d647f5fd3ec28
wyljpn <wyljpn@gmail.com>
2022-07-11 20:50:57 +0900

    Day 19. 145. Binary Tree Postorder Traversal

---
+++ Tree/145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,39 @@
+# Example 1:
+# Input: root = [1,null,2,3]
+# Output: [3,2,1]
+#
+# Example 2:
+#
+# Input: root = []
+# Output: []
+# Example 3:
+#
+# Input: root = [1]
+# Output: [1]
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+        res = []
+
+        def traversal(root):
+            if root == None:
+                return
+
+            traversal(root.left)
+            traversal(root.right)
+            res.append(root.val)
+
+        traversal(root)
+
+        return res
