f258cb27a02a1a75f9efe8b2916200e52b459d09
yulong.wang <wyljpn@gmail.com>
2022-07-23 17:03:34 +0900

    Day 22. 206. Reverse Linked List

---
+++ LinkedList/206. Reverse Linked List.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        cur = head
+        pre = None
+
+        while cur:
+            cur.next, cur, pre = pre, cur.next, cur
+
+        return pre
+
