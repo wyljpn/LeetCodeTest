5ccf702e163ab5cdd96586a7e7e577e23e08b773
yulong.wang <wyljpn@gmail.com>
2022-07-23 20:00:13 +0900

    Recursive way to reverse linked list

--- LinkedList/206. Reverse Linked List.py
+++ LinkedList/206. Reverse Linked List.py
@@ -3,6 +3,8 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution(object):
     def reverseList(self, head):
         """
@@ -18,3 +20,17 @@ class Solution(object):
 
         return pre # 此时cur已经是None，pre是最后一个item
 
+
+    def reverseList_2(self, head):
+        cur = head
+        pre = None
+
+        def reverse(cur, pre):
+            if not cur:
+                return pre
+
+            temp = cur.next
+            cur.next = pre
+            return reverse(temp, cur)
+
+        return reverse(cur, pre)
