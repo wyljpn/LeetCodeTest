1ab3205c0c282c2a0f86ecc0381f796fee82fe78
yulong.wang <wyljpn@gmail.com>
2022-08-13 22:48:19 +0900

    Day 26. 17. Letter Combinations of a Phone Number

---
+++ Backtractking/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,35 @@
+class Solution(object):
+
+    def __init__(self):
+        self.answers = []
+        self.answer = ''
+        self.letter_map = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+    def letterCombinations(self, digits):
+        if not digits: return []
+        self.backtracking(digits, 0)
+        return self.answers
+
+    def backtracking(self, digits, index):
+        # 回溯函数没有返回值
+        # Base Case
+        if index == len(digits):  # 当遍历穷尽后的下一层时
+            self.answers.append(self.answer)
+            return
+            # 单层递归逻辑
+        letters = self.letter_map[digits[index]]
+        for letter in letters:
+            self.answer += letter  # 处理
+            self.backtracking(digits, index + 1)  # 递归至下一层
+            self.answer = self.answer[:-1]  # 回溯
+
+
