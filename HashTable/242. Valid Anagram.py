cbf299d85b51cca8d64bc5e014bc8b6b876955e7
yulong.wang <wyljpn@gmail.com>
2022-08-14 16:44:33 +0900

    Use defailtdict

--- HashTable/242. Valid Anagram.py
+++ HashTable/242. Valid Anagram.py
@@ -18,3 +18,17 @@ class Solution(object):
                 return False
 
         return True
+
+
+    def isAnagram_2(self, s, t):
+        from collections import defaultdict
+        s_dic = defaultdict(int)
+        t_dic = defaultdict(int)
+
+        for char in s:
+            s_dic[char] += 1
+
+        for char in t:
+            t_dic[char] += 1
+
+        return s_dic == t_dic
