a6ce06afc3ccd7bf73ed5df7ad73f3181f4cfba3
yulong.wang <wyljpn@gmail.com>
2022-08-14 18:29:23 +0900

    Added reason why it's better to use Array to solve 383.

--- HashTable/383. Ransom Note.py
+++ HashTable/383. Ransom Note.py
@@ -1,3 +1,7 @@
+# 依然是数组在哈希法中的应用。
+#
+# 一些同学可能想，用数组干啥，都用map完事了，其实在本题的情况下，使用map的空间消耗要比数组大一些的，因为map要维护红黑树或者哈希表，而且还要做哈希函数，是费时的！数据量大的话就能体现出来差别了。 所以数组更加简单直接有效！
+
 class Solution(object):
     # 用Counter
     def canConstruct(self, ransomNote, magazine):
