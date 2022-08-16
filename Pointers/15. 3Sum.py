6e0d4181ac91187d51ce646fcc59d15d0bfd2de4
yulong.wang <wyljpn@gmail.com>
2022-08-16 16:33:15 +0900

    Changed directories name

--- Pointers/15. 3Sum.py
+++ Pointers/15. 3Sum.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+
+        # 限制条件：一个组合中，允许有相同的元素,[-1, -1, 2]。但是不允许有完全相同的组合, [-1, -1, 2] 和 [-1, -1, 2]。
+        # 在[-1, -1, 2, 2]中，result中只允许一个[-1, -1, 2]
         ans = []
         n = len(nums)
         nums.sort()
