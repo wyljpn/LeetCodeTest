d7c79a75bebf289653d7c75eb6fd0c5d0189d5c1
yulong.wang <wyljpn@gmail.com>
2022-08-19 20:05:24 +0900

    Day 30. 459. Repeated Substring Pattern. KMP

--- String/459. Repeated Substring Pattern.py
+++ String/459. Repeated Substring Pattern.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        for i in range(1, len(s)//2 +1):
+        for i in range(1, len(s)//2 + 1):
             if s[:i] * (len(s)//i) == s:
                 return True
         return False
@@ -14,7 +14,34 @@ class Solution(object):
     def repeatedSubstringPattern_1(self, s):
         return s in (2*s)[1:-1]
 
+    def repeatedSubstringPattern_2(self, s):
 
+        if not s:
+            return False
+
+        nxt = self.getNext(s)
+
+        if nxt[-1] != 0 and (len(s) % (len(s) - nxt[-1]) == 0):
+            return True
+
+        return False
+
+
+    def getNext(self, s):
+        nxt = [0] * len(s)
+
+        j = 0
+
+        for i in range(1, len(s)):
+            while j > 0 and s[i] != s[j]:
+                j = nxt[j - 1]
+
+            if s[i] == s[j]:
+                j += 1
+
+            nxt[i] = j
+
+        return nxt
 
 so = Solution()
 
