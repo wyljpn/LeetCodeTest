9211ed6c6c07ca471ae72f59fdfc7ca69b445edf
yulong.wang <wyljpn@gmail.com>
2022-08-30 22:41:37 +0900

    Added some comments

--- DynamicProgramming/416. Partition Equal Subset Sum.py
+++ DynamicProgramming/416. Partition Equal Subset Sum.py
@@ -28,17 +28,10 @@ class Solution:
 
         dp = [[0 for _ in range(target + 1)] for _ in range(len(nums))]
 
-        for i in range(len(nums)):
-            dp[i][0] = 0
-
-        for j in range(target + 1):
-            if j >= nums[0]:
-                dp[0][j] = nums[0]
-
         for i in range(1, len(nums)):
             for j in range(1, target + 1):
-                if j < nums[i]:
-                    dp[i][j] = dp[i - 1][j]
+                if j < nums[i]:  # 说明背包装不下当前物品.
+                    dp[i][j] = dp[i - 1][j]  # 所以不装当前物品,直接继承上一个值
                 else:
                     dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - nums[i]] + nums[i])
 
--- DynamicProgramming/WeightBagProblem.py
+++ DynamicProgramming/WeightBagProblem.py
@@ -4,21 +4,12 @@ class Solution(object):
         rows, cols = len(weight), bag_size + 1
         dp = [[0 for _ in range(cols)] for _ in range(rows)]
 
-        # 初始化dp数组.
-        for i in range(rows):
-            dp[i][0] = 0
-
-        first_item_weight, first_item_value = weight[0], value[0]
-        for j in range(cols):
-            if j >= first_item_weight:
-                dp[0][j] = first_item_value
-
         # 更新dp数组: 先遍历物品, 再遍历背包.
         for i in range(1, rows):
             cur_weight, cur_val = weight[i], value[i]
             for j in range(1, cols):
-                if j < cur_weight:
-                    dp[i][j] = dp[i - 1][j]  # 所以不装当前物品.
+                if j < cur_weight:  # 说明背包装不下当前物品.
+                    dp[i][j] = dp[i - 1][j]  # 所以不装当前物品,直接继承上一个值
                 else:
                     dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - cur_weight] + cur_val)
 
