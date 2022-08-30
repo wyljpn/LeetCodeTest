90b2c97ea6a09c81eef7eee59706a928f116983b
yulong.wang <wyljpn@gmail.com>
2022-08-30 22:17:42 +0900

    Added traditional way

--- DynamicProgramming/416. Partition Equal Subset Sum.py
+++ DynamicProgramming/416. Partition Equal Subset Sum.py
@@ -16,3 +16,25 @@ class Solution:
 
         return dp[-1] == target
 
+
+    def canPartition_2(self, nums):
+
+        target = sum(nums)
+
+        if target % 2 == 1:
+            return False
+
+        target //= 2
+
+        dp = [[0 for _ in range(target + 1)] for _ in range(len(nums))]
+
+        for j in range(target + 1):
+            if j >= nums[0]:
+                dp[0][j] = nums[0]
+
+        for i in range(len(nums)):
+            for j in range(target):
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - nums[i]] + nums[i])
+
+        return dp[-1] == target
+
