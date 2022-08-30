b854eeeaf3420fe69db0ed4bf40f455c1af21ed1
yulong.wang <wyljpn@gmail.com>
2022-08-30 22:38:06 +0900

    Corrected the traditional way

--- DynamicProgramming/416. Partition Equal Subset Sum.py
+++ DynamicProgramming/416. Partition Equal Subset Sum.py
@@ -28,13 +28,18 @@ class Solution:
 
         dp = [[0 for _ in range(target + 1)] for _ in range(len(nums))]
 
+        for i in range(len(nums)):
+            dp[i][0] = 0
+
         for j in range(target + 1):
             if j >= nums[0]:
                 dp[0][j] = nums[0]
 
-        for i in range(len(nums)):
-            for j in range(target):
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - nums[i]] + nums[i])
+        for i in range(1, len(nums)):
+            for j in range(1, target + 1):
+                if j < nums[i]:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - nums[i]] + nums[i])
 
-        return dp[-1] == target
-
+        return dp[-1][-1] == target
