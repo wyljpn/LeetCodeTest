78d30e6b667cefc1ae1fa2922e37fd50f175d21e
yulong.wang <wyljpn@gmail.com>
2022-09-03 16:46:03 +0900

    Day 40. 222. Count Complete Tree Nodes

---
+++ Tree/222. Count Complete Tree Nodes.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root):
+        import collections
+        queue = collections.deque()
+        if root:
+            queue.append(root)
+        result = 0
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                result += 1 #记录节点数量
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return result
