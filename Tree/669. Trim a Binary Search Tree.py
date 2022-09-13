7820edb884ddcac5eb8797c07d074e958506f3a5
yulong.wang <wyljpn@gmail.com>
2022-09-13 23:03:08 +0900

    Day 45. 669. Trim a Binary Search Tree

--- Tree/669. Trim a Binary Search Tree.py
+++ Tree/669. Trim a Binary Search Tree.py
@@ -17,3 +17,31 @@ class Solution(object):
         root.left = self.trimBST(root.left, L, R)
         root.right = self.trimBST(root.right, L, R)
         return root
+
+    def trimBST_2(self, root, low, high):
+        if not root:
+            return None
+
+        # 把root移动到[low, high]中
+        while root and (root.val < low or root.val > high):
+            if root.val < low:
+                root = root.right
+            else:
+                root = root.left
+
+        # 把root的左子树中小于low的部分移除
+        # 一直往左下角遍历，当遇到小于low的node，就检查那个node的右子树
+        cur = root
+        while cur:
+            while cur.left and cur.left.val < low:
+                cur.left = cur.left.right
+            cur = cur.left
+
+        # 把root的右子树中大于high的部分移除
+        cur = root
+        while cur:
+            while cur.right and cur.right.val > high:
+                cur.right = cur.right.left
+            cur = cur.right
+
+        return root
