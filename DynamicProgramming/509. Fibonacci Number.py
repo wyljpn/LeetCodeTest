2d374679cff9caef89ea2b5c3c44ca48414fe9b4
yulong.wang <wyljpn@gmail.com>
2022-09-14 22:57:43 +0900

    Added dp way

--- DynamicProgramming/509. Fibonacci Number.py
+++ DynamicProgramming/509. Fibonacci Number.py
@@ -17,7 +17,21 @@ class Solution(object):
         
         return dp[-1]
 
-    def fib(self, n):
+
+    def fib_2(self, n):
+
+        if n < 2:
+            return n
+
+        dp = [0 for _ in range(n + 1)]
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[-1]
+
+
+    def fib_3(self, n):
 
         if n < 2:
             return n
@@ -28,3 +42,4 @@ class Solution(object):
             dp[1], dp[0] = dp[0] + dp[1], dp[1]
 
         return dp[-1]
+
