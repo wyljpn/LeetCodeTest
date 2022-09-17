2b4c0060f91ba2213b07ee26863daeeb2c54996a
yulong.wang <wyljpn@gmail.com>
2022-09-17 10:18:22 +0900

    Made it more clear

--- DynamicProgramming/343. Integer Break.py
+++ DynamicProgramming/343. Integer Break.py
@@ -16,3 +16,16 @@ class Solution(object):
                 dp[i] = max(dp[i], j * (i-j), j * dp[i-j])
         
         return dp[-1]
+
+
+    def integerBreak_2(self, n):
+
+        dp = [0 for _ in range(n + 1)]
+
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], j * (i - j), dp[i - j] * j)
+
+        return dp[-1]
