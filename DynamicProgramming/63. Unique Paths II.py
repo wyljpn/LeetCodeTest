0b3282db378fb079200d69ad869513b1c9b36120
yulong.wang <wyljpn@gmail.com>
2022-09-17 09:44:57 +0900

    Day 47. Made it more clear

--- DynamicProgramming/63. Unique Paths II.py
+++ DynamicProgramming/63. Unique Paths II.py
@@ -1,27 +1,27 @@
 class Solution(object):
     def uniquePathsWithObstacles(self, obstacleGrid):
 
-        # dp = [[0] * len(obstacleGrid[0])] * len(obstacleGrid)
-        dp = [[0 for _ in range(len(obstacleGrid[0]))] for _ in range(len(obstacleGrid))]
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
 
-        for j in range(len(obstacleGrid[0])):
-            if obstacleGrid[0][j] != 1:
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        for j in range(n):
+            if not obstacleGrid[0][j]:
                 dp[0][j] = 1
             else:
                 break
 
-        for i in range(len(obstacleGrid)):
-            if obstacleGrid[i][0] != 1:
+        for i in range(m):
+            if not obstacleGrid[i][0]:
                 dp[i][0] = 1
             else:
                 break
 
-        for i in range(1, len(obstacleGrid)):
-            for j in range(1, len(obstacleGrid[0])):
-                if obstacleGrid[i][j] != 1:
+        for i in range(1, m):
+            for j in range(1, n):
+                if not obstacleGrid[i][j]:
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-                else:
-                    continue
 
         return dp[-1][-1]
 
