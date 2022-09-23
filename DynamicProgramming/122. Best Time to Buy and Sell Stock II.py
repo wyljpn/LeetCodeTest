d0c3de0e875fa4b8cf531b8c33fbccc711e99e99
yulong.wang <wyljpn@gmail.com>
2022-09-23 12:34:01 +0900

    Added some comments

--- DynamicProgramming/122. Best Time to Buy and Sell Stock II.py
+++ DynamicProgramming/122. Best Time to Buy and Sell Stock II.py
@@ -7,6 +7,9 @@ class Solution:
         dp[0][0] = - prices[0]
         dp[0][1] = 0
 
+
+        # dp[i][0] 表示第i天持有股票所得现金。取前天(股票价格+收益)和当天（股票价格+收益）的最低值。（因为取反了，所以是用max）。把完成交易后的收益加入到dp[i][0]中。
+        # dp[i][1] 表示第i天不持有股票所得最多现金。比较卖出股票之后的盈利，取max。
         for i in range(1, length):
             dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])
             dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
