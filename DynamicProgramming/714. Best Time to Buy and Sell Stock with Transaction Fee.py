730ddd2f91ef25e627bdaaff9d6f45cf3313b65c
yulong.wang <wyljpn@gmail.com>
2022-09-25 01:41:33 +0900

    Day 55. 714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ DynamicProgramming/714. Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices, fee):
+        n = len(prices)
+
+        dp = [[0, 0] for _ in range(n)]
+
+        dp[0][0] = - prices[0]
+        dp[0][1] = 0
+
+        for i in range(1, n):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i])
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee)
+
+        return max(dp[-1][0], dp[-1][1])
