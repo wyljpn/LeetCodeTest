c35655db78e6692d2161783be07f071162eb5257
yulong.wang <wyljpn@gmail.com>
2022-09-29 19:38:35 +0900

    Update scolling array solution, and added comments.

--- DynamicProgramming/718. Maximum Length of Repeated Subarray.py
+++ DynamicProgramming/718. Maximum Length of Repeated Subarray.py
@@ -25,17 +25,29 @@ class Solution:
 
     def findLength_2(self, nums1, nums2):
         # 使用滚动数组
-        dp = [0 for _ in range(len(nums2) + 1)]
+        n = len(nums1) + 1
+        m = len(nums2) + 1
 
+        # dp数组的意义
+        # 以下标i-1为结尾的nums1，和以下标j-1为结尾的nums2，最长重复子数组长度为dp[j]。
+        # （特别注意： “以下标i-1为结尾的A” 标明一定是 以nums1[i-1]为结尾的字符串 ）
+        dp = [0 for _ in range(m)]
+
+        # 初始化
+        # dp[:]都是0
+
+        # 因为最大值没有固定的位置，所以要用一个变量来比较和保存最大值
         result = 0
 
-        for i in range(1, len(nums1) + 1):
-            for j in range(len(nums2), 0, -1):
-                # 如果相等的话，就等于dp[i][j-1]+1
-                if nums1[i - 1] == nums2[j - 1]:
-                    dp[j] = dp[j - 1] + 1
+        for i in range(1, n):
+            # 前开后闭区间
+            for j in range(m-1, 0, -1):
+                if nums1[i-1] == nums2[j-1]:
+                    dp[j] = dp[j-1] + 1
                     result = max(dp[j], result)
+                # 因为使用滚动数组，当不想等的时候，要将item变成0。否则会影响后面的计算。
                 else:
                     dp[j] = 0
-            print(dp)
+        # print(dp)
         return result
+
