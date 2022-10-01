e2ae2a1f74df70c978b521b3079860d4e990ad45
yulong.wang <wyljpn@gmail.com>
2022-10-01 15:59:12 +0900

    Day 62. 72. Edit Distance

--- DynamicProgramming/72. Edit Distance.py
+++ DynamicProgramming/72. Edit Distance.py
@@ -46,6 +46,35 @@ class Solution(object):
 
         return dp[-1]
 
+    def minDistance_2(self, word1, word2):
+
+        n = len(word1) + 1
+        m = len(word2) + 1
+
+        # dp数组的意义
+        # 要使word1[:i]和word2[:j]相等，所需操作的字符的数量
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+
+        # 初始化
+        # 把word1[:i]中的字符都删掉，就能拿到一个空字符串，即与删掉出所有字符的word2相等
+        for i in range(n):
+            dp[i][0] = i
+
+        # 把word1[:i]中的字符都删掉，就能拿到一个空字符串，即与删掉出所有字符的word1相等
+        for j in range(m):
+            dp[0][j] = j
+
+        for i in range(1, n):
+            for j in range(1, m):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
+
+        # print(dp)
+
+        return dp[-1][-1]
+
 so = Solution()
 
 print(so.minDistance("", "a"))
