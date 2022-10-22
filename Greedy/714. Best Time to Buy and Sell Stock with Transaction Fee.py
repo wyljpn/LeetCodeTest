9edc30b2fb32a6a7f23944b4de5940b8d21a38ad
yulong.wang <wyljpn@gmail.com>
2022-10-22 15:15:25 +0900

    Day 72.

---
+++ Greedy/714. Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices, fee):
+
+        result = 0
+
+        minPrice = prices[0]
+
+        for i in range(1, len(prices)):
+            if prices[i] < minPrice:
+                minPrice = prices[i]
+            elif prices[i] >= minPrice and prices[i] <= (minPrice + fee):
+                continue
+            else:
+                result += prices[i] - minPrice - fee
+                minPrice = prices[i] - fee
+
+        return result
